3551cacf65921cc0da82e4c2ae54c231125378b1
TheHatter14147 <hibot1000@gmail.com>
2017-06-28 22:43:43 +1000

    fuck dis shit XD
    sleep time

--- C#/Zombot/Zombot/Zombot/Main.cs
+++ C#/Zombot/Zombot/Zombot/Main.cs
@@ -20,18 +20,80 @@ namespace Zombot
                 z.LogLevel = LogSeverity.Info;
                 z.LogHandler = log;
             });
+            discord.UsingCommands(z =>
+            {
+                z.PrefixChar = ';';
+                z.AllowMentionPrefix = true;
+            });
 
+
+            var commands = discord.GetService<CommandService>();
+
+
+
+            
+
+            commands.CreateCommand("start")
+            .Do(async (e) =>
+            {
+               
+                await e.Channel.SendMessage("Starting Zombies");
+                await Zom(e);
+            });
+
+
+
+           // commands.CreateCommand("test")
+                //.Do(async (e) =>
+                //{
+                    //var rules = e.Server.FindChannels("rules").FirstOrDefault();
+
+                  //  await rules.SendMessage("i can talk on differnet channels cause i'm not retarded");
+                   
+                //});
             discord.ExecuteAndWait(async () =>
             {
                 await discord.Connect("MzI5NDgxMDE4NzAyNTYxMjgz.DDTnvA.L0Ad9SCmqZmldTQCAVcqoS0Hti0", TokenType.Bot);
             });
 
-
+            
 
         }
         private void log(object sender, LogMessageEventArgs e)
         {
             Console.WriteLine(e.Message);
         }
+
+        private Task Zom(CommandEventArgs e)
+        {
+            discord.UsingCommands(z =>
+            {
+                z.PrefixChar = ';';
+                z.AllowMentionPrefix = true;
+            });
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("fire")
+          .Do(async (b) =>
+          {
+              await e.Channel.SendMessage("@Red Skull Raiders Zombies are attacking!!!!");
+
+              again:
+              int zombiecout = 1;
+              var msg = b.Server.FindChannels("red-skull-raiders").FirstOrDefault();
+              await msg.SendMessage(b.User.Name + "Hit a zombie");
+             if (zombiecout == 0)
+              {
+                  await msg.SendMessage("All zombies are dead");
+              }
+             else
+              {
+                  goto again;
+              }
+          });
+
+        }
+
+
     }
 }
