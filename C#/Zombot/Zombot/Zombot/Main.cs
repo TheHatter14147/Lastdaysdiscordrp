4469896d44de098fcdaf30b559393bc587cd009a
TheHatter14147 <hibot1000@gmail.com>
2017-06-29 19:48:21 +1000

    Command inside a command
    Command inside a command

--- C#/Zombot/Zombot/Zombot/Main.cs
+++ C#/Zombot/Zombot/Zombot/Main.cs
@@ -12,6 +12,7 @@ namespace Zombot
     class Main
     {
         DiscordClient discord;
+        
 
         public Main()
         {
@@ -31,26 +32,30 @@ namespace Zombot
 
 
 
-            
 
-            commands.CreateCommand("start")
-            .Do(async (e) =>
-            {
-               
-                await e.Channel.SendMessage("Starting Zombies");
-                await Zom(e);
+
+
+            var thecommand = commands.CreateCommand("CIAC");
+           
+
+            commands.CreateCommand("test")
+           .Do(async (e) =>
+           {
+               var channel = e.Server.FindChannels("red-skull-raiders").FirstOrDefault();
+
+               await channel.SendMessage("Enter cool text for rp here");
+               thecommand
+               .Do(async (d) =>
+               {
+
+                   await channel.SendMessage("Bam command inside of a command bitch!!!!");
+
+               });
+
             });
 
 
 
-           // commands.CreateCommand("test")
-                //.Do(async (e) =>
-                //{
-                    //var rules = e.Server.FindChannels("rules").FirstOrDefault();
-
-                  //  await rules.SendMessage("i can talk on differnet channels cause i'm not retarded");
-                   
-                //});
             discord.ExecuteAndWait(async () =>
             {
                 await discord.Connect("MzI5NDgxMDE4NzAyNTYxMjgz.DDTnvA.L0Ad9SCmqZmldTQCAVcqoS0Hti0", TokenType.Bot);
@@ -64,36 +69,8 @@ namespace Zombot
             Console.WriteLine(e.Message);
         }
 
-        private Task Zom(CommandEventArgs e)
-        {
-            discord.UsingCommands(z =>
-            {
-                z.PrefixChar = ';';
-                z.AllowMentionPrefix = true;
-            });
-            var commands = discord.GetService<CommandService>();
 
-            commands.CreateCommand("fire")
-          .Do(async (b) =>
-          {
-              await e.Channel.SendMessage("@Red Skull Raiders Zombies are attacking!!!!");
-
-              again:
-              int zombiecout = 1;
-              var msg = b.Server.FindChannels("red-skull-raiders").FirstOrDefault();
-              await msg.SendMessage(b.User.Name + "Hit a zombie");
-             if (zombiecout == 0)
-              {
-                  await msg.SendMessage("All zombies are dead");
-              }
-             else
-              {
-                  goto again;
-              }
-          });
-
-        }
-
+    
 
     }
 }
